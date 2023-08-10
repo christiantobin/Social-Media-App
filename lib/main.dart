3726828ad99e660d1b219d58c724fff42465e851
Christian Tobin <christian@wirelesseco.com>
2023-08-09 19:46:36 -0700

    Cleaner

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'login.dart';
 
 void main() {
@@ -8,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
