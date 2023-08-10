29b97191b10edeacf4d289c3f3ac2dc03482a47e
Christian Tobin <christian@wirelesseco.com>
2023-08-09 23:15:02 -0700

    Update to Smaller ranking

--- lib/homeContent/rankContent.dart
+++ lib/homeContent/rankContent.dart
@@ -6,7 +6,7 @@ class RankContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 5,
+      itemCount: 3,
       itemBuilder: (context, index) {
         return ListTile(
           onTap: () {
